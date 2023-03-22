b48970adeb1fc7c770b9a826495cc8f69b78f1ba
AzmiHwamed <70154469+AzmiHwamed@users.noreply.github.com>
2023-03-22 16:17:01 +0100

    Navigator added

--- src/main/java/com/example/messageapplication/Navigator/Navigator.java
+++ src/main/java/com/example/messageapplication/Navigator/Navigator.java
@@ -1,4 +1,33 @@
 package com.example.messageapplication.Navigator;
 
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.util.HashMap;
+
 public class Navigator {
+    static Navigator obj;
+    Stage stage;
+    HashMap<String,Scene> hashMap=new HashMap<>();
+    private Navigator() {
+    }
+    public static Navigator getInstance(){
+        if(obj==null)
+            obj=new Navigator();
+        return obj;
+    }
+
+    public void setStage(Stage stage) {
+        this.stage = stage;
+    }
+
+    public void addScene(String name, Scene scene){
+        this.hashMap.put(name,scene);
+    }
+    public void removeScene(String name){
+        this.hashMap.remove(name);
+    }
+    public void showScene(String name){
+        this.stage.setScene(obj.hashMap.get(name));
+    }
 }
